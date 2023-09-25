f90da6afb13329f4392d856abde7475dcf09b255
Sergey Trotsenko <advocate.tsv@gmail.com>
2023-09-25 17:54:55 +0600

    feat(Repository>Abstractions): Add empty specification, change Pagination.

---
+++ Repository/Repository.Abstractions/EmptySpecification.cs
@@ -0,0 +1,6 @@
+﻿namespace Tsw.Repository.Abstractions;
+
+public class EmptySpecification<TEntity> : Specification<TEntity>
+{
+  public override IQueryable<TEntity> ApplyToTable(IQueryable<TEntity> table) => table;
+}
--- Repository/Repository.Abstractions/Pagination.cs
+++ Repository/Repository.Abstractions/Pagination.cs
@@ -1,5 +1,17 @@
 ﻿namespace Tsw.Repository.Abstractions;
 
-public record Pagination(int Skip, int Take);
+public record Pagination
+{
+  private readonly int _skip;
+  public int Skip => _skip;
+  private readonly int _take;
+  public int Take => _take;
+
+  public Pagination(int page, int pageSize)
+  {
+    _skip = (page - 1) * pageSize;
+    _take = pageSize;
+  }
+}
 
 public record PaginationResult<TEntity>(int TotalCount, List<TEntity> Items);
