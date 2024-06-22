1d6dd5073f04a25463d91b8c45fc4d0fc9864182
Sergey Trotsenko <advocate.tsv@gmail.com>
2024-06-22 22:53:57 +0600

    update(Repository.Abstractions): physical path

--- Repository/Abstractions/Specification.cs
+++ Repository/Abstractions/Specification.cs
@@ -5,14 +5,14 @@ namespace Tsw.Repository.Abstractions;
 public abstract class Specification<TEntity>
 {
   public IReadOnlyCollection<Expression<Func<TEntity, object>>> Includes => _includes;
-  private readonly List<Expression<Func<TEntity, object>>> _includes = [];
+  private readonly List<Expression<Func<TEntity, object>>> _includes = new();
   protected void AddInclude(Expression<Func<TEntity, object>> navigationPropertyPath)
   {
     _includes.Add(navigationPropertyPath);
   }
 
   public IReadOnlyCollection<string> ThenIncludes => _thenIncludes;
-  private readonly List<string> _thenIncludes = [];
+  private readonly List<string> _thenIncludes = new();
   protected void AddThenInclude(string navigationPropertyPath) // for deep include
   {
     _thenIncludes.Add(navigationPropertyPath);
